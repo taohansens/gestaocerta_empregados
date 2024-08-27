d262a545a76c8cfa80eda8e300649e128db94238
taohansens <taohansens@gmail.com>
2024-08-27 10:08:54 -0300

    :pencil: docs: swagger EmpregadoController

--- src/main/java/com/taohansen/gestaocerta/controllers/EmpregadoController.java
+++ src/main/java/com/taohansen/gestaocerta/controllers/EmpregadoController.java
@@ -1,9 +1,11 @@
 package com.taohansen.gestaocerta.controllers;
 
+import com.taohansen.gestaocerta.controllers.exceptions.StandardError;
 import com.taohansen.gestaocerta.dtos.EmpregadoDTO;
 import com.taohansen.gestaocerta.entities.Empregado;
 import com.taohansen.gestaocerta.services.EmpregadoService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -25,8 +27,8 @@ public class EmpregadoController {
 
     @Operation(summary = "Retorna uma lista de todos empregados cadastrados")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Ok", content = {@Content(
-                    mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Empregado.class)))}
+            @ApiResponse(responseCode = "200", description = "Retorna a lista com os empregados cadastrados, vazia ou não", content = {@Content(
+                    mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = EmpregadoDTO.class)))}
             )})
     @GetMapping
     public ResponseEntity<List<EmpregadoDTO>> getAll() {
@@ -36,15 +38,23 @@ public class EmpregadoController {
 
     @Operation(summary = "Retorna o empregado pelo Id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK", content = {@Content(mediaType = "application/json",
-                    array = @ArraySchema(schema = @Schema(implementation = Empregado.class)))})
-    })
+            @ApiResponse(responseCode = "200", description = "Retorna o empregado com o respectivo ID", content = {@Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = EmpregadoDTO.class)))}),
+            @ApiResponse(responseCode = "404", description = "Empregado não localizado", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = StandardError.class))})})
     @GetMapping(value = "/{id}")
-    public ResponseEntity<EmpregadoDTO> findById(@PathVariable Long id) {
+    public ResponseEntity<EmpregadoDTO> findById(
+            @Parameter(description = "ID do empregado a ser buscado", required = true)
+            @PathVariable Long id) {
         EmpregadoDTO empregadoDTO = service.findById(id);
         return ResponseEntity.ok().body(empregadoDTO);
     }
 
+    @Operation(summary = "Criação de empregado.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created", content = {@Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = EmpregadoDTO.class)))})
+    })
     @PostMapping
     public ResponseEntity<EmpregadoDTO> insert(@RequestBody EmpregadoDTO dto) {
         dto = service.insert(dto);
@@ -53,14 +63,35 @@ public class EmpregadoController {
         return ResponseEntity.created(uri).body(dto);
     }
 
+    @Operation(summary = "Atualiza um empregado existente.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Empregado atualizado com sucesso", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = EmpregadoDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Empregado não encontrado", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = StandardError.class))})
+    })
     @PutMapping(value = "/{id}")
-    public ResponseEntity<EmpregadoDTO> update(@PathVariable Long id, @RequestBody EmpregadoDTO dto) {
+    public ResponseEntity<EmpregadoDTO> update(
+            @Parameter(description = "ID do empregado a ser atualizado", required = true)
+            @PathVariable Long id,
+            @Parameter(description = "Dados atualizados do empregado", required = true)
+            @RequestBody EmpregadoDTO dto) {
         EmpregadoDTO entity = service.update(id, dto);
         return ResponseEntity.ok().body(entity);
     }
 
+    @Operation(summary = "Deleta empregado pelo ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Empregado deletado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Operação não realizada, Empregado Não Encontrado", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = StandardError.class))}),
+            @ApiResponse(responseCode = "400", description = "Operação não realizada, inconsistência no Banco de Dados.", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = StandardError.class))})
+    })
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(
+            @Parameter(description = "ID do empregado a ser buscado", required = true)
+            @PathVariable Long id) {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
     }
